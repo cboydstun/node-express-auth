77126ac87892833db397551d4785a1ec7185a29c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-14 05:41:19 -0500

    connnected to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const PORT = 5000;
+dotenv.config();
 
-app.use('/', (req, res) => {
-    console.log("main url")
-})
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,})
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => {console.log(err);})
 
 app.listen(PORT, ()=>{console.log(`Server listening at ${PORT}`)})
