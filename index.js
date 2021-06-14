60f2d56d2e046d358f4aac47ca1e6a39df05d3e8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-14 09:59:50 -0500

    clean up

--- index.js
+++ index.js
@@ -14,5 +14,4 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 
 app.use('/auth', authRoute)
 
-
 app.listen(PORT, ()=>{console.log(`Server listening at ${PORT}`)})
