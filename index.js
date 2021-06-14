c2a859a41f6e0772594278d8e35cc22531475312
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-14 07:22:00 -0500

    added basic auth route

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth')
 
 const PORT = 5000;
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,})
     .then(console.log("Connected to MongoDB"))
     .catch((err) => {console.log(err);})
+
+app.use('/auth', authRoute)
 
 app.listen(PORT, ()=>{console.log(`Server listening at ${PORT}`)})
